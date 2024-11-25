dc28e94d8a14f8734a95719636aa1946a314659c
DylanMoeller <u16dm21@abdn.ac.uk>
2024-11-25 19:52:57 +0000

    working touch sensor log (no functionality as of now)

--- src/my_package/my_package/my_robot_driver.py
+++ src/my_package/my_package/my_robot_driver.py
@@ -17,10 +17,13 @@ class MyRobotDriver:
         self.__right_motor.setPosition(float('inf'))
         self.__right_motor.setVelocity(0)
 
+        self.__touch_sensor = self.__robot.getDevice('touch')
+        self.__touch_sensor.enable(int(self.__robot.getBasicTimeStep()))
+
         self.__target_twist = Twist()
 
         rclpy.init(args=None)
-        self.__node = rclpy.create_node('my_robot_driver1')
+        self.__node = rclpy.create_node('my_robot_driver')
         self.__node.create_subscription(Twist, 'cmd_vel1', self.__cmd_vel_callback, 1)
 
     def __cmd_vel_callback(self, twist):
@@ -28,6 +31,11 @@ class MyRobotDriver:
 
     def step(self):
         rclpy.spin_once(self.__node, timeout_sec=0)
+
+        touch_value = float(self.__touch_sensor.getValue())
+
+        if touch_value > 0.0:  
+            self.__node.get_logger().info('Bump Detected')
 
         forward_speed = self.__target_twist.linear.x
         angular_speed = self.__target_twist.angular.z

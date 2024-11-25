ac008573a4dceec183608bdf1f62b2765c61336f
Clementine <u23cc21@abdn.ac.uk>
2024-11-25 16:09:03 +0000

    Some test changes to touch sensor node

--- src/my_package/my_package/touch_sensor.py
+++ src/my_package/my_package/touch_sensor.py
@@ -8,27 +8,35 @@ from sensor_msgs.msg import Range
 class TouchSensor(Node):
     def __init__(self):
         super().__init__('touch_sensor')
+        self.robot = Robot()
         self.time_step = int(self.robot.getBasicTimeStep())
 
         self.touch_sensor = self.robot.getDevice('touch_sensor')
         self.touch_sensor.enable(self.time_step)
-        while self.robot.step(self.time_step) != -1:
-            touch_value = self.touch_sensor.getValue()
 
-            if touch_value > 0:  # Touch sensor value greater than 0 indicates contact
-                self.__node.get_logger().info('Bump detected')
-    
-    def callback(self, message):
-        self.__touch_sensor = message.range
+        self.timer = self.create_timer(0.1, self.check_touch_sensor)
 
+    def check_touch_sensor(self):
+        if self.robot.step(self.time_step) == -1:
+            self.get_logger().info("Simulation ended")
+            rclpy.shutdown()
+
+        touch_value = self.touch_sensor.getValue()
+
+        if touch_value > 0:
+            self.get_logger().info("Bump Detected")
 
                 
 def main(args=None):
     rclpy.init(args=None)
     touch_sensor = TouchSensor()
-    rclpy.spin(touch_sensor)
-    touch_sensor.destroy_node()
-    rclpy.shutdown()
+    try:
+        rclpy.spin(touch_sensor)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        touch_sensor.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()

c00cf42ee31c39f6b074968133638ee5fe37ba7a
DylanMoeller <u16dm21@abdn.ac.uk>
2024-11-26 22:03:02 +0000

    gps for robot2?

--- src/my_package/my_package/my_robot_driver2.py
+++ src/my_package/my_package/my_robot_driver2.py
@@ -1,6 +1,5 @@
 import rclpy
 from geometry_msgs.msg import Twist, Point
-from controller import Supervisor
 
 HALF_DISTANCE_BETWEEN_WHEELS = 0.045
 WHEEL_RADIUS = 0.025
@@ -8,8 +7,7 @@ WHEEL_RADIUS = 0.025
 class MyRobotDriver2:
     def init(self, webots_node, properties):
         self.__robot = webots_node.robot
-        self.__supervisor = Supervisor()
-        robot_node = self.__supervisor.getFromDef('my_robot_2')
+
         self.__left_motor = self.__robot.getDevice('left wheel motor')
         self.__right_motor = self.__robot.getDevice('right wheel motor')
 
@@ -19,25 +17,30 @@ class MyRobotDriver2:
         self.__right_motor.setPosition(float('inf'))
         self.__right_motor.setVelocity(0)
 
+        self.__gps = self.__robot.getDevice('gps')
+        self.__gps.enable(int(self.__robot.getBasicTimeStep()))
+
         self.__target_twist = Twist()
 
         rclpy.init(args=None)
         self.__node = rclpy.create_node('my_robot_driver2')
         self.__node.create_subscription(Twist, 'cmd_vel2', self.__cmd_vel_callback, 1)
-        self.__node.create_publisher(Point, 'robot2/position', self.__publish_position , 1)
+
+        self.__gps_publisher = self.__node.create_publisher(Point, '/robot2/gps', 10)
+        self.__timer = self.__node.create_timer(3.0, self.publish_gps)
 
     def __cmd_vel_callback(self, twist):
         self.__target_twist = twist
 
-    def __publish_position(self):
-        self.__node.get_logger().info('Publishing position')
-        position = Point()
-        position.x = self.__supervisor.getSelf().getPosition()[0]
-        position.y = self.__supervisor.getSelf().getPosition()[1]
-        position.z = self.__supervisor.getSelf().getPosition()[2]
-        self.__publisher.publish(position)
+    def publish_gps(self):
+        position = self.__gps.getValues()  
+        msg = Point()
+        msg.x = position[0]
+        msg.y = position[1]
+        msg.z = position[2]
+        self.__gps_publisher.publish(msg)
+        self.__node.get_logger().info(f'Published GPS Position: {msg}')
 
-    
     def step(self):
         rclpy.spin_once(self.__node, timeout_sec=0)
 
@@ -47,10 +50,24 @@ class MyRobotDriver2:
         command_motor_left = (forward_speed - angular_speed * HALF_DISTANCE_BETWEEN_WHEELS) / WHEEL_RADIUS
         command_motor_right = (forward_speed + angular_speed * HALF_DISTANCE_BETWEEN_WHEELS) / WHEEL_RADIUS
 
-        trans_field = robot_node.getField('translation')
-        while supervisor.step(self.__supervisor.getBasicTimeStep()) != -1:
-            values = trans_field.getSFVec3f()
-            self.__node.get_logger().info('Position: %f %f %f' % (values[0], values[1], values[2]))
-
         self.__left_motor.setVelocity(command_motor_left)
         self.__right_motor.setVelocity(command_motor_right)
+
+def main():
+    rclpy.init()
+    webots_node = None
+    properties = None   
+
+    driver = MyRobotDriver2()
+    driver.init(webots_node, properties)
+    try:
+        while True:
+            driver.step()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()

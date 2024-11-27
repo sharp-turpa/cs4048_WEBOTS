0b892ccd16fafffa4ff6426f8db2480bf19e3e39
DylanMoeller <u16dm21@abdn.ac.uk>
2024-11-27 20:15:05 +0000

    semi-working

--- src/my_package/my_package/flag_controller.py
+++ src/my_package/my_package/flag_controller.py
@@ -1,9 +1,9 @@
+import rclpy
 from controller import Robot, TouchSensor
 
 class FlagController:
-    def __init__(self, webots_node, properties):
-        
-        self.__robot = webots_node.robot
+    def __init__(self): 
+        self.robot = Robot()
         self.time_step = int(self.robot.getBasicTimeStep())
     
         self.touch_sensor = self.robot.getDevice('flagtouch')
@@ -12,20 +12,15 @@ class FlagController:
         rclpy.init(args=None)
         self.__node = rclpy.create_node('flag_controller')
 
-
-    def run(self):
+    def step(self):
         while self.robot.step(self.time_step) != -1:
             if self.touch_sensor.getValue() > 0:
                 self.__node.get_logger().info('Flag touched!')
 
-def main():
-    rclpy.init()
-    webots_node = None
-    properties = None
 
+def main():
     controller = FlagController()
-    controller.init(webots_node, properties)
-    controller.run()
+    controller.step()
 
 if __name__ == '__main__':
     main()
